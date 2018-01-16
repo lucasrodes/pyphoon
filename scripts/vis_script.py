7355fe1fb60abdf978d9ca92655b8e91613abb12
lucas rg <lucas@github.io>
2018-01-16 15:28:45 +0900

    visualization script. Provides animation of typhoon sequence (compatible for directory path with h5 files and whole array)

--- scripts/vis_script.py
+++ scripts/vis_script.py
@@ -1,10 +1,11 @@
 """
-This script generates an animation of a stream of frames. Useful to see the evolution of a typhoon
+This script generates an animation of a stream of frames. Useful to see the evolution of a typhoon sequence or a single
+typhoon image
 """
 
 
 from vis import DisplayImage
-import argparse
+from utils import read_h5file
 
 # PARSER
 """
@@ -13,7 +14,15 @@ parser.add_argument("directory", help="Add directory containing the hdf5 files")
 args = parser.parse_args()
 directory_path = args.directory
 """
-# CREATE OBJECT
-directory_path = '../data/201626/'
-visualization = DisplayImage(directory_path)
-visualization.run()
+option = 2
+
+if option == 1:
+    # OPTION 1
+    directory_path = 'data/image/201626/'
+    visualization = DisplayImage(directory=directory_path)
+    visualization.run()
+elif option == 2:
+    # OPTION 2
+    X, Y = read_h5file("197901.h5")
+    visualization = DisplayImage(data=X)
+    visualization.run()
