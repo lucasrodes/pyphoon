1cf8de6cfc79de0a94b9bd6bd84d3767ee8dba31
lucas rg <lucas@github.io>
2018-02-28 01:26:02 +0900

    sample data updated new format

--- pyphoon/visualize.py
+++ pyphoon/visualize.py
@@ -5,104 +5,101 @@ sequence or generate a GIF from it.
 
 Overall, this package is the perfect bridge between your stored data and your
 thoughts. Just bring data to life with it and start exploring!
+
+Getting Started
+^^^^^
+
+There are two options to visualize a sequence of frames. Either using a
+:class:`~pyphoon.utils.io.TyphoonList` instance
+
+>>> from pyphoon.io.typhoonlist import read_typhoonlist_h5
+>>> from pyphoon.visualize import DisplaySequence
+>>> # Load sequence from HDF file
+>>> path = "data/201626.h5"
+>>> typhoon_sequence = read_typhoonlist_h5(path_to_file=path)
+>>> DisplaySequence(
+    typhoon_sequence=typhoon_sequence,
+    interval=100,
+    start_frame=0,
+    end_frame=-1
+).run()
+
+or directly feeding a raw list of frames.
+
+>>> from pyphoon.io.h5 import read_h5file
+>>> from pyphoon.io.typhoonlist import read_typhoonlist_h5
+>>> from pyphoon.visualize import DisplaySequence
+>>> # Load sequence from HDF file
+>>> path = "data/201626.h5"
+>>> typhoon_sequence = read_typhoonlist_h5(path_to_file=path)
+>>> data = typhoon_sequence.images
+>>> DisplaySequence(
+    raw_data=data,
+    name="201626",
+    interval=100,
+    start_frame=0,
+    end_frame=-1
+).run()
+
+
+Methods and classes
+^^^^^
 """
 
 import matplotlib.pyplot as plt
 from matplotlib import animation
 
 
-############################
-#        VISUALISE         #
-############################
-
 class DisplaySequence(object):
     """ Used to animate a batch of images.
 
-    :var typhoon_sequence: :class:`~pyphoon.utils.io.TyphoonList` instance.
-    :var raw_data: List of frames.
-    :var name: Name of the sequence.
+    :var typhoon_sequence: :class:`~pyphoon.utils.io.typhoonlist.TyphoonList`
+        instance. Contains the data to bec displayed.
     :var interval: Interval between frames while visualizing the animation.
     :var start_frame: First frame of the sequence to visualize.
     :var end_frame: Last frame of the sequence to visualize.
-    :var axis: Set to True if axis are to be displayed.
     :var show_title: Set to false if no title should be shown in the figure.
-
-    |
-
-    :Example:
-
-        There are two options to visualize a sequence of frames. Either using a
-        :class:`~pyphoon.utils.io.TyphoonList` instance
-
-        >>> from pyphoon.io.typhoonlist import read_typhoonlist_h5
-        >>> from pyphoon.visualize import DisplaySequence
-        >>> # Load sequence from HDF file
-        >>> path = "data/201626.h5"
-        >>> typhoon_sequence = read_typhoonlist_h5(path_to_file=path)
-        >>> DisplaySequence(
-            typhoon_sequence=typhoon_sequence,
-            interval=100,
-            start_frame=0,
-            end_frame=-1
-        ).run()
-
-        or directly feeding a raw list of frames.
-
-        >>> from pyphoon.io.h5 import read_h5file
-        >>> from pyphoon.io.typhoonlist import read_typhoonlist_h5
-        >>> from pyphoon.visualize import DisplaySequence
-        >>> # Load sequence from HDF file
-        >>> path = "data/201626.h5"
-        >>> typhoon_sequence = read_typhoonlist_h5(path_to_file=path)
-        >>> data = typhoon_sequence.images
-        >>> DisplaySequence(
-            raw_data=data,
-            name="201626",
-            interval=100,
-            start_frame=0,
-            end_frame=-1
-        ).run()
     """
-    def __init__(self, typhoon_sequence=None, raw_data=None, name="untitled",
-                 interval=100, start_frame=0, end_frame=None, axis=False,
-                 show_title=True, alt_title=None):
+    def __init__(self, typhoon_sequence=None, interval=100, start_frame=0,
+                 end_frame=None, show_title=True, alt_title=None):
 
-        if typhoon_sequence is not None:
-            self.data = typhoon_sequence.data['X']
-            self.data_id = typhoon_sequence.data['X_ids']
-            match_best_image = [
-                flag == 1 for flag in typhoon_sequence.data['Y'][:, -2]
-            ]
-            self.flag_fix = typhoon_sequence.data['Y'][match_best_image, -1]
-            self.name = typhoon_sequence.name
-        elif raw_data is not None:
-            self.data = raw_data
-            self.data_id = ["no_id" for i in self.data]
-            self.flag_fix = ["no_flag" for i in self.data]
-            self.name = name
-        else:
-            raise Exception("Missing argument. Use either argument "
-                            "<typhoon_sequence> or <raw_data>")
-        if end_frame is None:
-            self.data = self.data[start_frame:]
-        else:
-            self.data = self.data[start_frame:end_frame]
+        # Load images
+        self.images = typhoon_sequence.get_data('images')
+        self.ids = typhoon_sequence.get_id('images')
+
+        # Load name
+        self.name = typhoon_sequence.name
+
+        # Define display window
         self.start_frame = start_frame
+        if end_frame is None:
+            self.images = self.images[start_frame:]
+        else:
+            self.images = self.images[start_frame:end_frame]
+
+        # Frame interval
         self.interval = interval
 
-        self.axis = axis
+        # Plot properties
         self.alt_title = alt_title
         self.show_title = show_title
         self.fig = plt.figure()
         self.ax = plt.gca()
-        if not self.axis:
-            plt.axis('off')
-        self.im = self.ax.imshow(self.data[0], cmap="Greys")
+        plt.axis('off')
+        self.im = self.ax.imshow(self.images[0], cmap="Greys")
+
+        """
+        # Other stuff idk
+        match_best_image = [
+            flag == 1 for flag in typhoon_sequence.data['Y'][:, -2]
+        ]
+        self.flag_fix = typhoon_sequence.data['Y'][match_best_image, -1]
+        """
 
     def _init(self):
         """ Resets initial image value
         """
-        self.im.set_data(self.data[0])
+        self.im.set_data(self.images[0])
 
     def _animate(self, i):
         """ Updates the image frame.
@@ -115,11 +112,11 @@ class DisplaySequence(object):
                 self.ax.set_title(self.alt_title)
             else:
                 self.ax.set_title(
-                    str(self.data_id[i]) + " | " +
-                    str(self.start_frame+i) + " | " +
-                    str(int(self.flag_fix[i]))
+                    str(self.ids[i]) + " | " +
+                    str(self.start_frame + i) #+ " | "
+                    # + str(int(self.flag_fix[i]))
                 )
-        self.im.set_data(self.data[i])
+        self.im.set_data(self.images[i])
 
     def run(self, save=False, filename="untitled"):
         """ Runs the animation
@@ -128,7 +125,7 @@ class DisplaySequence(object):
                                        func=self._animate,
                                        init_func=self._init,
                                        interval=self.interval,
-                                       frames=len(self.data),
+                                       frames=len(self.images),
                                        repeat=True
         )
 
@@ -144,6 +141,6 @@ class DisplaySequence(object):
                                        func=self._animate,
                                        init_func=self._init,
                                        interval=self.interval,
-                                       frames=len(self.data),
+                                       frames=len(self.images),
                                        repeat=True)
         return anim.to_html5_video()
